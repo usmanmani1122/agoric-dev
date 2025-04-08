25ae0ddbec373edb30712749c6852bdd61c1a01f
usmanmani1122 <usman.sbf@gmail.com>
2025-04-08 07:38:34 +0000

    feat: contract lifecycle with start and upgrade

---
+++ proposal-deployer/proposals/contract-lifecycle.js
@@ -0,0 +1,170 @@
+import { E } from "@endo/far";
+import { prepareExo } from "@agoric/vat-data";
+import { M } from "@agoric/store";
+
+const contractName = "contract-lifecycle";
+
+/**
+ * The first parameter will provide a `restoreRef` function which
+ * will add a mapping of bundle ID + bundle label which can be
+ * unwrapped to publish installations and consumed later
+ *
+ * @param {{
+ *  restoreRef: (ref: manifestBundleRef) => {
+ *      getBundle: () => Record<string, string>;
+ *      getBundleLabel: () => string;
+ *  }
+ * }} _
+ * @param {manifestBundleRef} __
+ */
+export const getManifest = ({ restoreRef }, { ref }) => ({
+  installations: {
+    [contractName]: restoreRef(ref),
+  },
+  manifest: {
+    [proposal.name]: {
+      consume: {
+        chainTimerService: true,
+        zoe: true,
+      },
+      installation: {
+        consume: { [contractName]: true },
+      },
+      instance: {
+        consume: { [contractName]: true },
+        produce: { [contractName]: true },
+      },
+    },
+  },
+  options: { ref },
+});
+
+/**
+ * First incarnation
+ *
+ * @param {{
+ *  consume: {
+ *      chainTimerService: Promise<import('@agoric/time').TimerService>;
+ *  }
+ * }} powers
+ * @param {import('@agoric/vat-data').Baggage} __
+ */
+export const proposal = ({
+  consume: { chainTimerService: chainTimerServicePromise, zoe },
+  installation: {
+    consume: { [contractName]: installation },
+  },
+  instance: {
+    produce: { [contractName]: produceInstance },
+  },
+}) =>
+  chainTimerServicePromise.then((chainTimerService) =>
+    E(zoe)
+      .startInstance(installation, {}, {}, { chainTimerService }, contractName)
+      .then((instance) => produceInstance.resolve(instance))
+  );
+
+// /**
+//  * Second incarnation
+//  *
+//  * @param {{
+//  *  consume: {
+//  *      chainTimerService: Promise<import('@agoric/time').TimerService>;
+//  *  }
+//  * }} powers
+//  * @param {import('@agoric/vat-data').Baggage} __
+//  */
+// export const proposal = (
+//   {
+//     consume: { chainTimerService: chainTimerServicePromise },
+//     instance: {
+//       consume: { [contractName]: contractInstancePromise },
+//     },
+//   },
+//   { options }
+// ) =>
+//   Promise.all([chainTimerServicePromise, contractInstancePromise]).then(
+//     ([chainTimerService, { adminFacet }]) =>
+//       E(adminFacet).upgradeContract(options.ref.bundleID, {
+//         chainTimerService,
+//         message: "Second incarnation invoked at time",
+//       })
+//   );
+
+/**
+ * First incarnation
+ *
+ * @param {any} _
+ * @param {{chainTimerService: import('@agoric/time').TimerService}} privateArgs
+ * @param {import('@agoric/vat-data').Baggage} baggage
+ */
+export const start = async (_, { chainTimerService, ...rest }, baggage) => {
+  assert(
+    !Object.keys(rest).length,
+    `Got unexpected parameters ${JSON.stringify(rest)}`
+  );
+
+  return {
+    publicFacet: prepareExo(
+      baggage,
+      "Public Facet",
+      M.interface("Contract Life Cycle", {
+        log: M.call().returns(M.promise()),
+      }),
+      {
+        /**
+         * @type {(...args: string) => Promise<void>}
+         */
+        log: (...args) =>
+          E(chainTimerService)
+            .getCurrentTimestamp()
+            .then((time) =>
+              console.log(
+                `First incarnation invoked at time "${Number(
+                  time.absValue
+                )}" with arguments: `,
+                ...args
+              )
+            ),
+      }
+    ),
+  };
+};
+
+// /**
+//  * Second incarnation
+//  *
+//  * @param {any} _
+//  * @param {{
+//  *  chainTimerService: import('@agoric/time').TimerService;
+//  *  message: string;
+//  * }} privateArgs
+//  * @param {import('@agoric/vat-data').Baggage} baggage
+//  */
+// export const start = async (_, { chainTimerService, message }, baggage) => {
+//   assert(!!message, "Expected message parameter");
+
+//   return {
+//     publicFacet: prepareExo(
+//       baggage,
+//       "Public Facet",
+//       M.interface("Contract Life Cycle", {
+//         log: M.call().returns(M.promise()),
+//       }),
+//       {
+//         /**
+//          * @type {(...args: string) => Promise<void>}
+//          */
+//         log: (...args) =>
+//           E(chainTimerService)
+//             .getCurrentTimestamp()
+//             .then((time) =>
+//               console.log(
+//                 `${message} "${Number(time.absValue)}" with arguments: `,
+//                 ...args
+//               )
+//             ),
+//       }
+//     ),
+//   };
+// };
