ab913e6bc97b8ecc89ef1411a84c640e4f749b6f
usmanmani1122 <usman.sbf@gmail.com>
2025-07-02 10:56:49 +0000

    chore: Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,62 +1,79 @@
+import { fixupConfigRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
 import prettier from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
-// eslint-disable-next-line import/no-unresolved
 import tseslint from 'typescript-eslint';
 
-const config = tseslint.config({
-  extends: [
-    importPlugin.flatConfigs.recommended,
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-  ],
-  files: ['**/*.js', '**/*.ts', '**/*.tsx'],
-  ignores: ['node_modules'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+const flatCompat = new FlatCompat();
+
+const config = tseslint.config(
+  {
+    ignores: ['**/.next', 'node_modules'],
   },
-  plugins: { prettier },
-  rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-empty-object-type': 'warn',
-    '@typescript-eslint/no-unnecessary-type-constraint': 'warn',
-    '@typescript-eslint/no-unsafe-function-type': 'warn',
-    '@typescript-eslint/no-unused-expressions': 'off',
-    '@typescript-eslint/no-wrapper-object-types': 'warn',
-    curly: ['error', 'multi'],
-    'import/namespace': 'off',
-    'import/no-named-as-default': 0,
-    'import/order': [
-      'error',
-      {
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
+  {
+    settings: {
+      next: {
+        rootDir: '*/',
+      },
+    },
+  },
+  ...fixupConfigRules(flatCompat.extends('eslint-config-next')),
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: { prettier, 'react-refresh': reactRefresh },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-unnecessary-type-constraint': 'warn',
+      '@typescript-eslint/no-unsafe-function-type': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-wrapper-object-types': 'warn',
+      curly: ['error', 'multi'],
+      'import/namespace': 'off',
+      'import/no-named-as-default': 0,
+      'import/order': [
+        'error',
+        {
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['sibling', 'parent'],
+            'index',
+            'unknown',
+          ],
+          'newlines-between': 'always',
         },
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          ['sibling', 'parent'],
-          'index',
-          'unknown',
-        ],
-        'newlines-between': 'always',
-      },
-    ],
-    'jsx-quotes': 0,
-    'no-constant-condition': ['error', { checkLoops: true }],
-    'no-use-before-define': 'off',
-    'no-void': [
-      'error',
-      {
-        allowAsStatement: true,
-      },
-    ],
-    'prettier/prettier': 'warn',
+      ],
+      'jsx-quotes': 0,
+      'no-constant-condition': ['error', { checkLoops: true }],
+      'no-use-before-define': 'off',
+      'no-void': [
+        'error',
+        {
+          allowAsStatement: true,
+        },
+      ],
+      'prettier/prettier': 'warn',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
   },
-});
+);
 
 export default config;
